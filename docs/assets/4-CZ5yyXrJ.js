7417df71b37ca759ca6830e90f87325ada2cb938
xchkups <test.com>
2024-12-29 20:11:47 +0800

    add docs

---
+++ docs/assets/4-CZ5yyXrJ.js
@@ -0,0 +1,1 @@
+import{bt as e,a3 as t,a2 as a}from"./index-DRo4SWcT.js";const c={};function n(o,r){return a(),t("div",null," Navigation 4 ")}const _=e(c,[["render",n]]);export{_ as default};
---
+++ docs/assets/item-1-3WLGv8kE.js
@@ -0,0 +1,1 @@
+import{bt as e,a3 as t,a2 as c}from"./index-DRo4SWcT.js";const n={};function r(a,o){return c(),t("div",null," Item One ")}const _=e(n,[["render",r]]);export{_ as default};
